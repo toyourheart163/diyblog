80f3fb715761068a0f342ce2db7b91ce24033b13
toyourheart163 <blive200@gmail.com>
2019-07-04 09:06:23 +0800

    suibian

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('blogs/', views.BlogListView.as_view(), name='blogs'),
+    path('bloggers/', views.BloggerListView.as_view(), name='bloggers'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import generic
+from .models import Blog, BlogAuthor
 
 def index(request):
     return render(request, 'index.html')
+
+class BlogListView(generic.ListView):
+    model = Blog
+    paginate_by = 5
+
+class BloggerListView(generic.ListView):
+    model = BlogAuthor
+    paginate_by = 5
